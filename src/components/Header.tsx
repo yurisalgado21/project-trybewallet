0fda5b58ddebf9e413e80e2f6084e63d204368b4
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-08-30 21:31:58 -0300

    Exercício 2 feito

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,22 @@
+import { useSelector } from 'react-redux';
+
+type UserType = {
+  user: {
+    email: string,
+  }
+};
+
 function Header() {
-  return <div>Header</div>;
+  const email = useSelector((state: UserType) => state.user.email);
+
+  return (
+    <>
+      <div>Header</div>
+      <p data-testid="email-field">{email}</p>
+      <h3 data-testid="total-field">0</h3>
+      <p data-testid="header-currency-field">BRL</p>
+    </>
+  );
 }
 
 export default Header;
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,12 @@
+import Header from '../components/Header';
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+  return (
+    <div>
+      TrybeWallet
+      <Header />
+    </div>
+  );
 }
 
 export default Wallet;
